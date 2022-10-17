90d8e41d80b767a8852fc477b55cba12a56851fe
Samuel D' Siqueira <samueldsiqueira@gmail.com>
2022-10-17 04:29:58 -0300

    fix: :pencil2: delete register function
    delet register function for refact

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -5,80 +5,61 @@ import apiGateway from '../services/apiGateway';
 export const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-	const [user, setUser] = useState(null);
-	const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-	const navigate = useNavigate();
-	const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	useEffect(() => {
-		const loadUser = async () => {
-			const token = localStorage.getItem('@hub-token:token');
+  useEffect(() => {
+    const loadUser = async () => {
+      const token = localStorage.getItem('@hub-token:token');
 
-			if (token) {
-				try {
-					apiGateway.defaults.headers.Authorization = `Bearer ${token}`;
+      if (token) {
+        try {
+          apiGateway.defaults.headers.Authorization = `Bearer ${token}`;
 
-					const { data } = await apiGateway.get('/profile');
+          const { data } = await apiGateway.get('/profile');
 
-					setUser(data);
-				} catch (error) {
-					console.error(error);
-				}
-			}
+          setUser(data);
+        } catch (error) {
+          console.error(error);
+        }
+      }
 
-			setLoading(false);
-		};
-		loadUser();
-	}, []);
+      setLoading(false);
+    };
+    loadUser();
+  }, []);
 
-	const registerUser = async (data) => {
-		try {
-			const response = await apiGateway.post('/sessions', data);
 
-			console.log(response);
 
-			const { user: userResponse, token } = response.data;
+  const loginUser = async (data) => {
+    try {
+      const response = await apiGateway.post('/sessions', data);
 
-			apiGateway.defaults.headers.authorization = `Bearer ${token}`;
+      const { user: userResponse, token } = response.data;
 
-			setUser(userResponse);
-			localStorage.setItem('@hub-token:token', token);
+      apiGateway.defaults.headers.authorization = `Bearer ${token}`;
 
-			const toNavigate = location.state?.ftom.pathname || '/dashboard';
+      setUser(userResponse);
+      localStorage.setItem('@hub-token:token', token);
 
-			navigate(toNavigate, { replace: true });
-		} catch (error) {
-			console.log(error);
-		}
-	};
+      const toNavigate = location.state?.from.pathname || '/dashboard';
 
-	const loginUser = async (data) => {
-		try {
-			const response = await apiGateway.post('/sessions', data);
+      navigate(toNavigate, { replace: true });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-			const { user: userResponse, token } = response.data;
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
 
-			apiGateway.defaults.headers.authorization = `Bearer ${token}`;
-
-			setUser(userResponse);
-			localStorage.setItem('@hub-token:token', token);
-
-			const toNavigate = location.state?.from.pathname || '/dashboard';
-
-			navigate(toNavigate, { replace: true });
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const logout = () => {
-		setUser(null);
-		localStorage.removeItem('token');
-		navigate('/login');
-	};
-
-	return <AuthContext.Provider value={{ user, loading, registerUser, logout }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ user, loading, registerUser, logout }}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
